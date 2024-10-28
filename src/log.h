8358b641b8494815515d65ec40bf18e6e8cc977a
Paul Wassermann <paul9499@gmail.com>
2024-10-28 23:01:12 +0100

    refacto: change logging format

--- src/log.h
+++ src/log.h
@@ -3,18 +3,36 @@
 
 #include <stdio.h>
 
-#define DEBUG 0
-#define INFO 1
-#define ERROR 2
+#define DEBUG_LEVEL 0
+#define INFO_LEVEL 1
+#define ERROR_LEVEL 2
+
+#define DEBUG_TAG "DEBUG"
+#define INFO_TAG "INFO"
+#define ERROR_TAG "ERROR"
 
 #define LOG_LEVEL DEBUG
 
-#define LOG_FORMAT "File: %s, line: %d, function: %s => "
-#define LOG_ARGS __FILE__, __LINE__, __FUNCTION__
+#define LOG_FORMAT "[%s][%s][%s] Line %d :: "
+#define LOG_ARGS(TAG) TAG, __FILE__, __FUNCTION__, __LINE__
 
-#if LOG_LEVEL <= ERROR
+#if LOG_LEVEL <= DEBUG_LEVEL
+#define LOG_DEBUG(format, ...) \
+    fprintf(stdout, LOG_FORMAT format, LOG_ARGS(DEBUG_TAG), __VA_ARGS__)
+#else
+#define LOG_DEBUG(format, ...)
+#endif
+
+#if LOG_LEVEL <= INFO_LEVEL
+#define LOG_INFO(format, ...) \
+    fprintf(stdout, LOG_FORMAT format, LOG_ARGS(INFO_TAG), __VA_ARGS__)
+#else
+#define LOG_INFO(format, ...)
+#endif
+
+#if LOG_LEVEL <= ERROR_LEVEL
 #define LOG_ERROR(format, ...) \
-    fprintf(stderr, LOG_FORMAT format, LOG_ARGS, __VA_ARGS__)
+    fprintf(stderr, LOG_FORMAT format, LOG_ARGS(ERROR_TAG), __VA_ARGS__)
 #else
 #define LOG_ERROR(format, ...)
 #endif
