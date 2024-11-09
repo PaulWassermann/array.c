0d73e5443f12fd31e340cfbb3c7d03ea87601b19
Paul Wassermann <paul9499@gmail.com>
2024-11-09 19:34:54 +0100

    feat: add pointer argument validation

--- src/array.c
+++ src/array.c
@@ -3,19 +3,12 @@
 
 #include "array.h"
 #include "log.h"
-
-// UTILS (TO MOVE)
-void compute_strides(Array *arr) {
-    size_t acc = 1;
-    for (size_t dim = arr->ndim - 1; dim < arr->ndim; dim--) {
-        arr->strides[dim] = acc;
-        acc *= arr->shape[dim];
-    }
-}
+#include "utils.h"
 
 // INITIALIZATION FUNCTIONS
 Array array(size_t *shape, size_t ndim, DType dtype, void *data) {
-
+    CHECK_NULL_PTR(shape);
+    
     Array out = {0};
 
     size_t nelems = reduce_mul(shape, ndim);
@@ -53,10 +46,16 @@ Array array(size_t *shape, size_t ndim, DType dtype, void *data) {
 }
 
 Array empty(size_t *shape, size_t ndim, DType dtype) {
+    CHECK_NULL_PTR(shape);
+
     return array(shape, ndim, dtype, NULL);
 }
 
 void free_array(Array arr) {
+    CHECK_NULL_DATA_PTR(&arr);
+    CHECK_NULL_SHAPE_PTR(&arr);
+    CHECK_NULL_STRIDES_PTR(&arr);
+
     free(arr.data);
     free(arr.shape);
     free(arr.strides);
@@ -72,13 +71,18 @@ size_t get_element_size(DType dtype) {
     case float32:
         return sizeof(float);
     default:
-        LOG_ERROR("Unsupported DType %d", dtype);
+        LOG_ERROR("Unsupported DType %d\n", dtype);
         exit(EXIT_FAILURE);
     }
 }
 
 // LOGIC INDEXING ⇄ MEMORY INDEXING
 size_t array_index_to_memory(size_t *array_index, Array *arr) {
+    CHECK_NULL_PTR(array_index);
+    CHECK_NULL_PTR(arr);
+
+    CHECK_NULL_STRIDES_PTR(arr);
+
     size_t memory_index = 0;
     for (int dim = 0; dim < arr->ndim; dim++) {
         memory_index += array_index[dim] * arr->strides[dim];
@@ -87,6 +91,9 @@ size_t array_index_to_memory(size_t *array_index, Array *arr) {
 }
 
 size_t *memory_index_to_array(size_t memory_index, Array *arr) {
+    CHECK_NULL_PTR(arr);
+    CHECK_NULL_STRIDES_PTR(arr);
+
     size_t *array_index = SAFE_MALLOC(sizeof(*array_index) * arr->ndim);
 
     for (size_t dim = 0; dim < arr->ndim; dim++) {
@@ -98,36 +105,46 @@ size_t *memory_index_to_array(size_t memory_index, Array *arr) {
 }
 
 Array view(size_t *shape, size_t ndim, Array *arr) {
+    CHECK_NULL_PTR(shape);
+    CHECK_NULL_PTR(arr);
+
     size_t nelems = reduce_mul(shape, ndim);
 
     if (nelems != arr->nelems) {
-        LOG_ERROR("Cannot view array with %zu elements as shape %s",
-                  arr->nelems, stringify_shape(shape, ndim, 1024));
+        LOG_ERROR("Cannot view array with %zu elements as shape %s\n",
+                  arr->nelems, get_iter_str(shape, ndim, 1024));
         exit(EXIT_FAILURE);
     }
 
     return array(shape, ndim, arr->dtype, arr->data);
 }
 
-// COMPUTE FUNCTIONS
-// ...
-
 // PRINT FUNCTIONS
 void print_info(Array *arr) {
+    CHECK_NULL_PTR(arr);
+    
     printf("Size of Array: %zu bytes\n",
            get_element_size(arr->dtype) * arr->nelems);
-    printf("The initialized array has %zu dimensions and %zu elements\n",
-           arr->ndim, arr->nelems);
+    printf("The array has %zu dimensions and %zu elements\n", arr->ndim,
+           arr->nelems);
 
-    char *shape_str = stringify_shape(arr->shape, arr->ndim, 1024);
-    printf("The shape of the array is: %s", shape_str);
+    printf("Array has dtype %s\n", get_dtype_str(arr->dtype));
 
-    free(shape_str);
+    if (arr->shape != NULL) {
+        char *shape_str = get_iter_str(arr->shape, arr->ndim, 1024);
+        printf("The shape of the array is: %s\n", shape_str);
+        free(shape_str);
+    } else {
+        printf("The shape of the array is: NULL\n");
+    }
+    
 
-    // TODO: would it be prettier if formatted like the shape array ?
-    printf("The stride for each dimension of the array are:\n");
-
-    for (size_t dim = 0; dim < arr->ndim; dim++) {
-        printf("\tDim %zu: %zu\n", dim, arr->strides[dim]);
+    if (arr->strides != NULL) {
+        printf("The stride for each dimension of the array are:\n");
+        for (size_t dim = 0; dim < arr->ndim; dim++) {
+            printf("\tDim %zu: %zu\n", dim, arr->strides[dim]);
+        }
+    } else {
+        printf("The stride for each dimension of the array are: NULL\n");
     }
 }
--- src/array.h
+++ src/array.h
@@ -1,8 +1,6 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-#include "utils.h"
-
 typedef enum DType { float32, int32 } DType;
 
 typedef struct Array {
