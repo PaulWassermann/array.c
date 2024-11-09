9965492cfb2130434cfab4bf41207323f97920e0
Paul Wassermann <paul9499@gmail.com>
2024-11-09 14:26:49 +0100

    feat: add macros for checking pointers validity

--- src/utils.c
+++ src/utils.c
@@ -1,34 +1,73 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "array.h"
 #include "log.h"
 #include "utils.h"
 
-// Array utils
-size_t reduce_mul(size_t *arr, size_t length) {
-    size_t out = 1;
-    for (size_t i = 0; i < length; i++) {
-        out *= arr[i];
+void compute_strides(Array *arr) {
+    CHECK_NULL_SHAPE_PTR(arr);
+    CHECK_NULL_STRIDES_PTR(arr);
+
+    size_t acc = 1;
+    for (size_t dim = arr->ndim - 1; dim < arr->ndim; dim--) {
+        arr->strides[dim] = acc;
+        acc *= arr->shape[dim];
     }
-    return out;
 }
 
-// Add input validation for buffer size (must be >= 4 because at least 4 bytes
-// will be written to it)
-char *stringify_shape(size_t *shape, size_t ndim, size_t buffer_size) {
+bool equal_shapes(Array *a, Array *b) {
+    CHECK_NULL_SHAPE_PTR(a);
+    CHECK_NULL_SHAPE_PTR(b);
+
+    if (a->ndim != b->ndim) {
+        return false;
+    }
+
+    for (size_t dim = 0; dim < a->ndim; dim++) {
+        if (a->shape[dim] != b->shape[dim]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+char *get_dtype_str(DType dtype) {
+    switch (dtype) {
+    case float32:
+        return "float32";
+    case int32:
+        return "int32";
+    default:
+        LOG_ERROR("DType %d does not exist\n", dtype);
+        exit(EXIT_FAILURE);
+    }
+}
+
+char *get_iter_str(size_t *iter, size_t length, size_t buffer_size) {
+    CHECK_NULL_PTR(iter);
+
+    if (buffer_size <= 6) {
+        LOG_ERROR("Buffer must be have at least 6 bytes available (received "
+                  "size %zu)\n",
+                  buffer_size);
+    }
+
     char *shape_buff = SAFE_MALLOC(sizeof(*shape_buff) * buffer_size);
     size_t bytes = 0;
 
-    bytes += snprintf(shape_buff + bytes, buffer_size, "(%zu,", shape[0]);
+    bytes += snprintf(shape_buff + bytes, buffer_size, "(%zu,", iter[0]);
 
-    for (size_t dim = 1; dim < ndim - 1; dim++) {
-        bytes += snprintf(shape_buff + bytes, buffer_size - bytes, " %zu,",
-                          shape[dim]);
+    for (size_t i = 1; i < length - 1; i++) {
+        bytes +=
+            snprintf(shape_buff + bytes, buffer_size - bytes, " %zu,", iter[i]);
     }
 
-    if (ndim > 1) {
+    if (length > 1) {
         bytes += snprintf(shape_buff + bytes, buffer_size - bytes, " %zu",
-                          shape[ndim - 1]);
+                          iter[length - 1]);
     }
 
     if (bytes >= buffer_size) {
@@ -44,7 +83,16 @@ char *stringify_shape(size_t *shape, size_t ndim, size_t buffer_size) {
     return shape_buff;
 }
 
-// Memory utils
+size_t reduce_mul(size_t *arr, size_t length) {
+    CHECK_NULL_PTR(arr);
+
+    size_t out = 1;
+    for (size_t i = 0; i < length; i++) {
+        out *= arr[i];
+    }
+    return out;
+}
+
 void *safe_malloc(size_t size, char *file, int line, const char *function) {
     void *ptr = malloc(size);
 
--- src/utils.h
+++ src/utils.h
@@ -1,13 +1,58 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdbool.h>
+
+#include "array.h"
+#include "log.h"
+
+#define CHECK_EQUAL_DTYPES(a, b)                                               \
+    if ((a)->dtype != (b)->dtype) {                                            \
+        LOG_ERROR("Arrays " #a " and " #b                                      \
+                  " have different dtypes: %s and %s\n",                       \
+                  get_dtype_str((a)->dtype), get_dtype_str((b)->dtype));       \
+        exit(EXIT_FAILURE);                                                    \
+    }
+#define CHECK_EQUAL_SHAPES(a, b)                                               \
+    if (!equal_shapes(a, b)) {                                                 \
+        LOG_ERROR("Arrays " #a " and " #b                                      \
+                  " have different shapes: %s and %s\n",                       \
+                  get_iter_str((a)->shape, (a)->ndim, 1024),                   \
+                  get_iter_str((b)->shape, (b)->ndim, 1024));                  \
+        exit(EXIT_FAILURE);                                                    \
+    }
+#define CHECK_NULL_PTR(ptr) \
+    if ((ptr) == NULL) { \
+        LOG_ERROR("Pointer %s is NULL\n", #ptr); \
+        exit(EXIT_FAILURE); \
+    }
+#define CHECK_NULL_DATA_PTR(arr) CHECK_NULL_PTR((arr)->data)
+#define CHECK_NULL_SHAPE_PTR(arr) CHECK_NULL_PTR((arr)->shape)
+#define CHECK_NULL_STRIDES_PTR(arr) CHECK_NULL_PTR((arr)->strides)
+
 #define SAFE_MALLOC(size) safe_malloc(size, __FILE__, __LINE__, __FUNCTION__)
 
-// Array utils
-size_t reduce_mul(size_t *arr, size_t length);
-char *stringify_shape(size_t *shape, size_t ndim, size_t buffer_size);
+/*Compute the strides of the given array, with respect to its `shape` and `ndim`
+members.
 
-// Memory utils
+For a given dimension, its stride is the product of the number of elements in 
+each of its inner dimensions (the rightmost dimensions).
+
+### Parameters
+
+#### InOut :
+- arr: the array for which the strides are to be computed
+
+### Panics
+If the `shape` member is a NULL pointer.
+*/
+void compute_strides(Array *arr);
+
+bool equal_shapes(Array *a, Array *b);
+char *get_dtype_str(DType dtype);
+char *get_iter_str(size_t *iter, size_t length, size_t buffer_size);
+size_t reduce_mul(size_t *arr, size_t length);
+
 void *safe_malloc(size_t size, char *file, int line, const char *function);
 
 #endif
